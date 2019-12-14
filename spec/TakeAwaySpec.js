2d296d840fb00fb519bdc687a1febad73a5892f4
mona lisa <sibaj@hotmail.co.uk>
2019-12-14 12:01:29 +0000

    failing test

---
+++ spec/TakeAwaySpec.js
@@ -0,0 +1,15 @@
+"use strict";
+
+describe("TakeAway", function) {
+  var TakeAway;
+
+  beforeEach(function () {
+    takeaway = new TakeAway();
+  });
+describe("it has a list of dishes", function() {
+  // it("has dishes with prices", function() {
+    dishes = {squid:7.90, prawns:8.90, friedrice:7.20, sprite:1.10};
+    takeaway.pickDishes(dishes);
+    expect(menu.listDishes()).toEqual(dishes);
+  });
+// });
